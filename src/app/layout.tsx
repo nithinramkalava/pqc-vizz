024e0cef648ad91d2be87cdc460fb406628fda57
Nithin Ram Kalava <nithinramkalava@gmail.com>
2025-03-21 17:11:07 +0530

    Add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import PageTransition from "./components/PageTransition";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ 
   subsets: ["latin"],
@@ -28,6 +29,7 @@ export default function RootLayout({
           </div>
         </PageTransition>
       </body>
+      <Analytics />
     </html>
   );
 }
